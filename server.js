5f1589f2d8923b8a7edbe9c5690b42a5d7a692d8
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-26 12:25:16 +0100

    Lagt till "use strict" för att aktivera strikt läge i db.js och server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const Hapi = require('@hapi/hapi'); // Importera Hapi
 const connectToDB = require('./config/db'); // Importera db-anslutning
 require('dotenv').config(); // Läs miljövariabler från .env
