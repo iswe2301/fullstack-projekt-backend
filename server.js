b3119af1e18945731544ae807d49ccc4fd46ab53
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-26 15:15:01 +0100

    Lagt till autentisering och CORS-konfiguration i serverinställningarna

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const Hapi = require('@hapi/hapi'); // Importera Hapi
 const connectToDB = require('./config/db'); // Importera db-anslutning
 require('dotenv').config(); // Läs miljövariabler från .env
+const auth = require('./auth'); // Importera autentisering
 
 const init = async () => {
     // Anslut till databasen
@@ -12,7 +13,22 @@ const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['http://localhost:3000', 'https://www.thunderclient.com', 'http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:5000'],
+                credentials: true,
+                maxAge: 86400,
+                headers: ["Accept", "Content-Type", "Access-Control-Allow-Origin"]
+            }
+        },
     });
+
+    // Registrera autentisering
+    await auth.register(server);
+
+    // Registrera routes
+    require('./routes/user.route')(server);
+    require('./routes/product.route')(server);
 
     // Starta servern
     await server.start();
